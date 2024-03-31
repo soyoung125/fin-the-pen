cd6feb4051154f18754d294200840cbd7cf4dba8
soyoung125 <sypark041754@gmail.com>
2024-03-31 21:15:48 +0900

    refactor: 일정 관리 페이지 제거와 지출 목표 페이지 추가

--- src/app/routes/MANAGEMENT_ROUTES.tsx
+++ src/app/routes/MANAGEMENT_ROUTES.tsx
@@ -3,9 +3,9 @@ import RegularDepositWithdrawal from "../../legacies/assetManagement/RegularDepo
 import DetailInformation from "../../legacies/assetManagement/RegularDepositWithDrawalContainer/detailPage/DetailInformation";
 import DetailSetting from "../../legacies/assetManagement/RegularDepositWithDrawalContainer/detailPage/DetailSetting";
 import SavingsGoal from "../../legacies/assetManagement/SavingsGoalContainer/SavingsGoal.tsx";
-import ScheduleManagement from "../../legacies/assetManagement/ScheduleManagement";
 import { PATH } from "@constants/path.ts";
 import { RouterDOM } from "@app/types/common.ts";
+import SpendingGoal from "@legacies/assetManagement/SpendingGoal";
 
 const MANAGEMENT_ROUTES: RouterDOM[] = [
   {
@@ -30,7 +30,7 @@ const MANAGEMENT_ROUTES: RouterDOM[] = [
   },
   {
     path: PATH.scheduleManagement,
-    element: <ScheduleManagement />,
+    element: <SpendingGoal />,
   },
 ];
 
