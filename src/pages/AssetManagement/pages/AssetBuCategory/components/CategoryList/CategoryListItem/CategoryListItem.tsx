3153927216d32a99df159be1f95acb10cfd1ff4e
soyoung125 <sypark041754@gmail.com>
2024-04-16 13:47:55 +0900

    feat: 카테고리 자산 설정 지출 목표 설정을 위한 input 태그 추가

--- src/pages/AssetManagement/pages/AssetBuCategory/components/CategoryList/CategoryListItem/CategoryListItem.tsx
+++ src/pages/AssetManagement/pages/AssetBuCategory/components/CategoryList/CategoryListItem/CategoryListItem.tsx
@@ -1,9 +1,11 @@
 import { Avatar, Stack, Typography, Collapse, Button } from "@mui/material";
 import { CATEGORY_ICONS } from "@components/ScheduleList/constants.ts";
-import { useState } from "react";
+import { ChangeEvent, MouseEvent, useState } from "react";
 import drop_up from "@assets/icons/drop_up.svg";
 import drop_down from "@assets/icons/drop_down.svg";
 import { Category } from "@app/types/asset.ts";
+import ListItemAction from "@pages/AssetManagement/pages/AssetBuCategory/components/CategoryList/CategoryListItem/components/ListItemAction";
+import ListItemHeader from "@pages/AssetManagement/pages/AssetBuCategory/components/CategoryList/CategoryListItem/components/ListItemHeader";
 
 export interface CategoryListItemProps {
   category: string;
@@ -23,33 +25,49 @@ function CategoryListItem({
   handleSubmit,
 }: CategoryListItemProps) {
   const [open, setOpen] = useState(false);
+  const [modifyTotal, setModifyTotal] = useState(false);
+  const [total, setTotal] = useState(amount);
+  const [form, setForm] = useState(categoryDetail);
+  const [control, setControl] = useState<string[]>([]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setForm(form.map((c) => (c.name === id ? { name: id, value: value } : c)));
+  };
+
+  const handleChangeTotal = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setTotal(parseInt(value));
+  };
+
+  const handleClickCancel = () => {
+    setForm(categoryDetail);
+    setControl([]);
+    setTotal(amount);
+    setModifyTotal(false);
+  };
+
+  const handleClickTotal = (e: MouseEvent<HTMLSpanElement>) => {
+    e.stopPropagation();
+    setModifyTotal(true);
+    setOpen(true);
+  };
 
   return (
     <>
-      <Stack
-        direction="row"
-        alignItems="center"
-        spacing={1.5}
-        p={2}
-        onClick={() => setOpen((prevState) => !prevState)}
-        bgcolor={open ? "#EAE1FD" : "#fff"}
-        borderBottom="1px solid #F7F7F8"
-      >
-        <Avatar
-          alt="category icon"
-          src={CATEGORY_ICONS[subCategories[0]]}
-          sx={{ width: 42, height: 42 }}
-        >
-          {category}
-        </Avatar>
-        <Typography variant="h4" sx={{ flexGrow: 1 }}>
-          {category}
-        </Typography>
-        <Typography variant="h5">{amount.toLocaleString()}원</Typography>
-        <img src={open ? drop_up : drop_down} alt="drop" />
-      </Stack>
+      <ListItemHeader
+        category={category}
+        mainSubCategory={subCategories[0]}
+        open={open}
+        total={total}
+        modifyTotal={modifyTotal}
+        setOpen={setOpen}
+        handleClickTotal={handleClickTotal}
+        handleChangeTotal={handleChangeTotal}
+      />
+
       <Collapse in={open}>
-        {categoryDetail.map((c) => (
+        {form.map((c) => (
           <Stack
             direction="row"
             alignItems="center"
@@ -61,33 +79,26 @@ function CategoryListItem({
             <Typography variant="h4">
               <li>{c.name}</li>
             </Typography>
-            <Typography
-              variant="h5"
-              color={c.value === "?" ? "#8C919C" : "#131416"}
-            >
-              {c.value === "?" ? c.value : Number(c.value).toLocaleString()}원
-            </Typography>
+            {control.includes(c.name) ? (
+              <input id={c.name} value={c.value} onChange={handleChange} />
+            ) : (
+              <Typography
+                variant="h5"
+                color={c.value === "?" ? "#8C919C" : "#131416"}
+                onClick={() => setControl(control.concat(c.name))}
+              >
+                {c.value === "?" ? c.value : Number(c.value).toLocaleString()}원
+              </Typography>
+            )}
           </Stack>
         ))}
 
-        <Stack direction="row" spacing={1} px={3} pt={1}>
-          <Button
-            variant="contained"
-            color="secondary"
-            fullWidth
-            onClick={handleCancel}
-          >
-            취소
-          </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            fullWidth
-            onClick={handleSubmit}
-          >
-            확인
-          </Button>
-        </Stack>
+        {(modifyTotal || control.length !== 0) && (
+          <ListItemAction
+            handleCancel={handleClickCancel}
+            handleSubmit={handleSubmit}
+          />
+        )}
       </Collapse>
     </>
   );
---
+++ src/pages/AssetManagement/pages/AssetBuCategory/components/CategoryList/CategoryListItem/components/ListItemHeader/ListItemHeader.tsx
@@ -0,0 +1,60 @@
+import { Avatar, Stack, Typography } from "@mui/material";
+import { CATEGORY_ICONS } from "@components/ScheduleList/constants.ts";
+import drop_up from "@assets/icons/drop_up.svg";
+import drop_down from "@assets/icons/drop_down.svg";
+import { ChangeEvent, Dispatch, MouseEvent, SetStateAction } from "react";
+
+export interface ListItemHeaderProps {
+  category: string;
+  mainSubCategory: string;
+  open: boolean;
+  modifyTotal: boolean;
+  total: number;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  handleChangeTotal: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleClickTotal: (e: MouseEvent<HTMLSpanElement>) => void;
+}
+
+function ListItemHeader({
+  mainSubCategory,
+  category,
+  modifyTotal,
+  setOpen,
+  total,
+  open,
+  handleChangeTotal,
+  handleClickTotal,
+}: ListItemHeaderProps) {
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      spacing={1.5}
+      p={2}
+      onClick={() => setOpen((prevState) => !prevState)}
+      bgcolor={open ? "#EAE1FD" : "#fff"}
+      borderBottom="1px solid #F7F7F8"
+    >
+      <Avatar
+        alt="category icon"
+        src={CATEGORY_ICONS[mainSubCategory]}
+        sx={{ width: 42, height: 42 }}
+      >
+        {category}
+      </Avatar>
+      <Typography variant="h4" sx={{ flexGrow: 1 }}>
+        {category}
+      </Typography>
+      {modifyTotal ? (
+        <input value={total} onChange={handleChangeTotal} />
+      ) : (
+        <Typography variant="h5" onClick={handleClickTotal}>
+          {total.toLocaleString()}원
+        </Typography>
+      )}
+      <img src={open ? drop_up : drop_down} alt="drop" />
+    </Stack>
+  );
+}
+
+export default ListItemHeader;
