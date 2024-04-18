746ef1f27dcbce516daaac4f059f07efe3632932
soyoung125 <sypark041754@gmail.com>
2024-04-18 23:45:46 +0900

    fix: CategoryListItem.stories.tsx 매개변수 누락으로 인한 빌드 오류 해결

--- src/pages/AssetManagement/pages/AssetBuCategory/components/CategoryList/CategoryListItem/CategoryListItem.stories.tsx
+++ src/pages/AssetManagement/pages/AssetBuCategory/components/CategoryList/CategoryListItem/CategoryListItem.stories.tsx
@@ -2,21 +2,34 @@ import CategoryListItem, {
   CategoryListItemProps,
 } from "@pages/AssetManagement/pages/AssetBuCategory/components/CategoryList/CategoryListItem/CategoryListItem.tsx";
 import { Meta } from "@storybook/react";
-import { EXPENDITURE_FOOD_CATEGORY } from "@components/ScheduleDrawer/pages/ScheduleFormPage/components/CategoryPicker/constants.ts";
+import {
+  EXPENDITURE_CATEGORY,
+  EXPENDITURE_FOOD_CATEGORY,
+} from "@components/ScheduleDrawer/pages/ScheduleFormPage/components/CategoryPicker/constants.ts";
 
 const meta = {
   title: "AssetManagement/AssetByCategory/CategoryList/CategoryListItem",
   component: CategoryListItem,
   tags: ["autodocs"],
   args: {
-    category: "음식",
-    subCategories: EXPENDITURE_FOOD_CATEGORY,
-    categoryDetail: [
-      { name: "식비", value: "1000000" },
-      { name: "카페", value: "10000" },
-      { name: "술", value: "?" },
-    ],
-    amount: 100000000,
+    category: EXPENDITURE_CATEGORY[0],
+    categoryList: {
+      category_name: EXPENDITURE_CATEGORY[0].name,
+      category_total: "1010000",
+      list: [
+        { name: "식비", value: "1000000" },
+        {
+          name: "카페",
+          value: "10000",
+        },
+        {
+          name: "술",
+          value: "?",
+        },
+      ],
+    },
+    control: "",
+    totalAmount: 100000000,
     handleSubmit: () => alert("submit"),
   },
   argTypes: {},
