9578b3a86fa2b7e1d8982c33f352e18235eb7c30
soyoung125 <sypark041754@gmail.com>
2024-05-21 14:41:13 +0900

    refactor: 저축 목표 페이지 저출 현황 카드 디자인 변경

--- src/pages/AssetManagement/pages/SavingGoal/SavingsGoal.tsx
+++ src/pages/AssetManagement/pages/SavingGoal/SavingsGoal.tsx
@@ -1,10 +1,11 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Stack, Box } from "@mui/material";
 import Saving from "@pages/AssetManagement/pages/SavingGoal/components/Saving/Saving.tsx";
 import { useUser } from "@app/tanstack-query/useUser.ts";
 import useSavingGoal from "@hooks/assetManagement/useSavingGoal.ts";
 import React from "react";
 import GoalSetting from "@pages/AssetManagement/pages/SavingGoal/components/GoalSetting/GoalSetting.tsx";
 import { useSettingGoalDrawer } from "@pages/AssetManagement/pages/SavingGoal/components/GoalSetting/SettingDrawer/useSettingDrawer.tsx";
+import { TotalSavingCard } from "@pages/AssetManagement/pages/SavingGoal/SavingGoal.styles.ts";
 
 function SavingsGoal() {
   const { data: user } = useUser();
@@ -17,23 +18,16 @@ function SavingsGoal() {
   };
 
   return (
-    <Box px={2.5} pt={2.5}>
-      <Stack justifyContent="space-between" spacing="7px">
-        <Typography variant="h2">
-          <span style={{ fontSize: "20px", color: "#735BF2", fontWeight: 500 }}>
-            {user?.name}
-          </span>
-          님의 저축 목표 입니다.
-        </Typography>
-
-        <Typography variant="h2">
+    <Stack px={2.5} pt={2.5} spacing={2.5}>
+      <Box>
+        <TotalSavingCard>
           {"오늘까지 총 "}
-          <span style={{ fontSize: "20px", color: "#735BF2", fontWeight: 500 }}>
+          <span style={{ fontSize: "18px", color: "#735BF2", fontWeight: 500 }}>
             900,000
           </span>
           {"원을 저축했어요."}
-        </Typography>
-      </Stack>
+        </TotalSavingCard>
+      </Box>
 
       <GoalSetting name={user?.name} handleModify={handleModify} />
 
@@ -46,7 +40,7 @@ function SavingsGoal() {
       {/*  personal={goal?.personal_goal}*/}
       {/*  handleSetPersonalGoal={handleSetPersonalGoal}*/}
       {/*/>*/}
-    </Box>
+    </Stack>
   );
 }
 
--- src/pages/AssetManagement/pages/SavingGoal/components/Saving/Saving.tsx
+++ src/pages/AssetManagement/pages/SavingGoal/components/Saving/Saving.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack } from "@mui/material";
+import { Stack } from "@mui/material";
 import RoundedPaper from "../../../../../../components/common/RoundedPaper.tsx";
 import { MonthSavingGoal } from "@app/types/asset.ts";
 import { getAmount } from "@pages/AssetManagement/utils.ts";
@@ -11,20 +11,18 @@ interface SavingProps {
 
 function Saving({ saving }: SavingProps) {
   return (
-    <Box mt="30px">
-      <RoundedPaper>
-        <Stack spacing="26px">
-          <Goal
-            title={"한 해 저축 목표"}
-            amount={getAmount(saving?.years_goal_amount)}
-          />
-          <Goal
-            title={"월 저축 목표"}
-            amount={getAmount(saving?.months_goal_amount)}
-          />
-        </Stack>
-      </RoundedPaper>
-    </Box>
+    <RoundedPaper>
+      <Stack spacing="26px">
+        <Goal
+          title={"한 해 저축 목표"}
+          amount={getAmount(saving?.years_goal_amount)}
+        />
+        <Goal
+          title={"월 저축 목표"}
+          amount={getAmount(saving?.months_goal_amount)}
+        />
+      </Stack>
+    </RoundedPaper>
   );
 }
 
