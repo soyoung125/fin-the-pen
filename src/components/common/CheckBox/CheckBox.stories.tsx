09fb76a63564b15a3735c89da07bc1c51d8a0135
soyoung125 <sypark041754@gmail.com>
2024-05-28 18:30:37 +0900

    feat: checkbox 커스텀 컴포넌트 구현

---
+++ src/components/common/CheckBox/CheckBox.stories.tsx
@@ -0,0 +1,47 @@
+import CheckBox from "@components/common/CheckBox/index.ts";
+import { Meta } from "@storybook/react";
+import { useState } from "react";
+import { FormControlLabel, FormGroup } from "@mui/material";
+import { CheckBoxProps } from "@components/common/CheckBox/CheckBox.tsx";
+
+const meta = {
+  title: "common/CheckBox",
+  component: CheckBox,
+  tags: ["autodocs"],
+  args: { checked: true },
+  argTypes: {},
+} satisfies Meta<typeof CheckBox>;
+
+export default meta;
+
+export const Default = (args: CheckBoxProps) => {
+  return <CheckBox {...args} />;
+};
+
+export const Example = () => {
+  const [selected, setSelected] = useState<number[]>([]);
+
+  const handleChange = (num: number) => {
+    if (selected.includes(num)) {
+      setSelected(selected.filter((s) => s !== num));
+    } else {
+      setSelected(selected.concat(num));
+    }
+  };
+
+  return (
+    <FormGroup>
+      {Array.from({ length: 3 }, (_, i) => i + 1).map((num) => (
+        <FormControlLabel
+          control={
+            <CheckBox
+              checked={selected.includes(num)}
+              handleChange={() => handleChange(num)}
+            />
+          }
+          label={`number ${num}`}
+        />
+      ))}
+    </FormGroup>
+  );
+};
