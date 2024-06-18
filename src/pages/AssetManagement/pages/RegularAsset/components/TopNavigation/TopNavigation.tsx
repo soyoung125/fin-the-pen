f091812cf1aa8d77c20bc4b7cd67559d437269e5
soyoung125 <sypark041754@gmail.com>
2024-06-18 15:41:19 +0900

    refactor: 정기 템플릿 관리 페이지 수정 아이콘 제거
    - 기능정의서에서 삭제된 수정 아이콘은 제거

--- src/pages/AssetManagement/pages/RegularAsset/components/TopNavigation/TopNavigation.tsx
+++ src/pages/AssetManagement/pages/RegularAsset/components/TopNavigation/TopNavigation.tsx
@@ -1,10 +1,8 @@
-import { Box } from "@mui/material";
 import { HeaderContainer } from "@components/TemplateDrawer/components/Header/Header.styles.ts";
 import BackButton from "@components/layouts/common/TopBar/buttons/BackButton.tsx";
 import useAsset from "@hooks/assetManagement/useAsset.ts";
 import assetManagements from "@constants/managements.ts";
 import SelectAssetMenu from "@components/layouts/common/TopBar/SelectAssetMenu";
-import ModifyIcon from "@assets/icons/modify.svg";
 
 function TopNavigation() {
   const { assetMenu, setMenu } = useAsset();
@@ -18,10 +16,6 @@ function TopNavigation() {
         setSelectedOption={setMenu}
         options={assetManagements.map((a) => a.title)}
       />
-
-      <Box flexGrow={1} textAlign="end">
-        <img src={ModifyIcon} alt="modify" />
-      </Box>
     </HeaderContainer>
   );
 }
