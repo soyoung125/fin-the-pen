9d8b8f356b648834d71c1368007b90cb5aeb8940
soyoung125 <sypark041754@gmail.com>
2024-07-30 09:07:26 +0900

    refactor: /asset/template/delete api 연동
    - request data를 query string으로 전달하도록 수정

--- src/app/tanstack-query/templates/useDeleteTemplate.ts
+++ src/app/tanstack-query/templates/useDeleteTemplate.ts
@@ -2,18 +2,17 @@ import { getSessionStorage } from "@utils/storage.ts";
 import { SESSION_STORAGE_KEY_TOKEN } from "@api/keys.ts";
 import { DOMAIN } from "@api/url.ts";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { QUERY_KEY_TEMPLATE } from "@constants/queryKeys.ts";
+import { QUERY_KEY_TEMPLATE_BY_PRICE_TYPE } from "@constants/queryKeys.ts";
 
-const fetchDeleteTemplate = async (id: string) => {
+const fetchDeleteTemplate = async (ids: string[]) => {
   const token = getSessionStorage(SESSION_STORAGE_KEY_TOKEN, "");
 
-  return fetch(`${DOMAIN}/asset/template/delete`, {
+  return fetch(`${DOMAIN}/asset/template/delete?${ids.join("&")}`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
       Authorization: "Bearer " + token,
     },
-    body: JSON.stringify({ template_id: id }),
   });
 };
 
@@ -23,15 +22,13 @@ export const useDeleteTemplate = () => {
     mutationFn: fetchDeleteTemplate,
     onSuccess: () => {
       queryClient.invalidateQueries({
-        queryKey: [QUERY_KEY_TEMPLATE],
+        queryKey: [QUERY_KEY_TEMPLATE_BY_PRICE_TYPE],
       });
     },
   });
 
   const deleteTemplate = async (templates: number[]) => {
-    templates.forEach((template) => {
-      mutate(template.toString());
-    });
+    mutate(templates.map((template) => `template_ids=${template}`));
   };
 
   return { deleteTemplate };
