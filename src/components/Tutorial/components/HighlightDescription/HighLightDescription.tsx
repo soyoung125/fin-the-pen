e0e9e70e86ed3dd60dab9906b7b71d2115ced015
soyoung125 <sypark041754@gmail.com>
2024-08-05 15:03:32 +0900

    refactor: HighLightDescription 디자인 변경

--- src/components/Tutorial/components/HighlightDescription/HighLightDescription.tsx
+++ src/components/Tutorial/components/HighlightDescription/HighLightDescription.tsx
@@ -1,10 +1,12 @@
 import {
   ArrowIcon,
+  ArrowIconContainer,
   Container,
   TextBox,
 } from "@components/Tutorial/components/HighlightDescription/HighLightDescription.styles.ts";
 import arrowUp from "@assets/icons/arrowUp.svg";
 import arrowDown from "@assets/icons/arrowDown.svg";
+import { Stack } from "@mui/material";
 
 export interface HighLightDescriptionProps {
   position: "top" | "bottom";
@@ -21,7 +23,15 @@ function HighLightDescription({
     <Container>
       {position === "bottom" && <ArrowIcon src={arrowUp} alt="arrowUp" />}
       <TextBox $isCenter={isCentered ?? false}>{message}</TextBox>
-      {position === "top" && <ArrowIcon src={arrowDown} alt="arrowDown" />}
+      {position === "top" &&
+        (isCentered ? (
+          <ArrowIconContainer>
+            <ArrowIcon src={arrowDown} alt="arrowDown" />
+            <ArrowIcon src={arrowDown} alt="arrowDown" />
+          </ArrowIconContainer>
+        ) : (
+          <ArrowIcon src={arrowDown} alt="arrowDown" />
+        ))}
     </Container>
   );
 }
