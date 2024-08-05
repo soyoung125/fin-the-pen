cdcfdc5b4bc4c9cd026f4c3976dee9a00037d5fe
soyoung125 <sypark041754@gmail.com>
2024-08-05 15:03:26 +0900

    refactor: HighLightDescription 디자인 변경

--- src/components/Tutorial/components/HighlightDescription/HighLightDescription.stories.tsx
+++ src/components/Tutorial/components/HighlightDescription/HighLightDescription.stories.tsx
@@ -1,4 +1,7 @@
-import { HighLightDescription } from "@components/Tutorial/components/HighlightDescription/HighLightDescription.tsx";
+import {
+  HighLightDescription,
+  HighLightDescriptionProps,
+} from "@components/Tutorial/components/HighlightDescription/HighLightDescription.tsx";
 import { Meta } from "@storybook/react";
 import { Box } from "@mui/material";
 
@@ -6,19 +9,26 @@ const meta = {
   title: "common/Tutorial/HighLightDescription",
   component: HighLightDescription,
   tags: ["autodocs"],
-  args: {},
-  argTypes: {},
+  args: { position: "top", message: "튜토리얼을 위한 설명글" },
+  argTypes: {
+    position: {
+      description: "설명글을 표시할 위치를 나타냅니다.",
+    },
+    message: {
+      description: "표시할 설명글을 의미합니다.",
+    },
+    isCentered: {
+      description: "설명을 가운데 정렬시킬 지 왼쪽으로 정렬시킬지 의미합니다.",
+    },
+  },
 } satisfies Meta<typeof HighLightDescription>;
 
 export default meta;
 
-export const Default = () => {
+export const Default = (args: HighLightDescriptionProps) => {
   return (
     <Box height={150} bgcolor={"#000"}>
-      <HighLightDescription
-        position={"top"}
-        message={"튜토리얼을 위한 설명글"}
-      />
+      <HighLightDescription {...args} />
     </Box>
   );
 };
