c45a1fd62d1d5212925c57b05c3cb86d41b88f90
soyoung125 <sypark041754@gmail.com>
2024-08-07 00:27:25 +0900

    refactor: HighLight 디자인 수정

--- src/components/Tutorial/Tutorial.styles.ts
+++ src/components/Tutorial/Tutorial.styles.ts
@@ -6,7 +6,7 @@ export const Container = styled.div`
   box-sizing: border-box;
   padding: 45px 0;
   mix-blend-mode: hard-light;
-  background-color: rgba(19, 19, 19, 0.8);
+  background-color: rgba(19, 19, 19, 0.9);
   position: fixed;
   top: -0px;
   z-index: 5000;
