66c8353afc7341aad6d2ba501e7a8154a5ed0dc8
soyoung125 <sypark041754@gmail.com>
2024-08-27 00:05:21 +0900

    fix: 일정 수정 오류 해결

--- src/hooks/assetManagement/RegularTemplate/useRegularAsset.ts
+++ src/hooks/assetManagement/RegularTemplate/useRegularAsset.ts
@@ -2,11 +2,9 @@ import { useUser } from "@app/tanstack-query/useUser.ts";
 import { useState } from "react";
 import { useDatePicker } from "@hooks/date-picker/hooks/useDatePicker.tsx";
 import moment from "moment";
-import { Schedule } from "@app/types/schedule.ts";
 import { useParams } from "react-router-dom";
 import { useTemplateByPriceType } from "@app/tanstack-query/templates/useTemplateByPriceType.ts";
 import { useDeleteTemplate } from "@app/tanstack-query/templates/useDeleteTemplate.ts";
-import { useDialog } from "@hooks/dialog/useDialog.tsx";
 
 const useRegularAsset = () => {
   const today = moment();
@@ -18,15 +16,6 @@ const useRegularAsset = () => {
   const { eventName, category } = useParams();
 
   const { data: user } = useUser();
-  // const {
-  //   data: schedules,
-  //   isError,
-  //   isPending,
-  // } = useRegularAssets({
-  //   user_id: user?.user_id ?? "",
-  //   start_date: period.start,
-  //   end_date: period.end,
-  // });
   const { data, isPending, isError } = useTemplateByPriceType({
     user_id: user?.user_id ?? "",
   });
@@ -37,46 +26,11 @@ const useRegularAsset = () => {
     (_, i) => (moment(period.start).year() + i).toString()
   );
 
-  const isDuplicated = (result: Schedule[], curr: Schedule) =>
-    result.find((s) => s.event_name === curr.event_name);
-
   const spendSchedules = data?.deposit ?? [];
 
   const saveSchedules = data?.withdraw ?? [];
 
   const templateCount = spendSchedules.length + saveSchedules.length;
-
-  const detailSchedules: Schedule[] = [
-    {
-      schedule_id: "1",
-      user_id: "test1234",
-      event_name: "가족들과의 식사",
-      category: "외식",
-      start_date: "2024-02-02",
-      end_date: "2024-02-02",
-      start_time: "18:00",
-      end_time: "21:00",
-      all_day: false,
-      repeat_options: {
-        term: "2",
-        options: "FRIDAY",
-      },
-      period: {
-        repeat_number_time: "3",
-        repeat_end_line: "2030-02-15",
-        is_repeat_again: false,
-      },
-      price_type: "-",
-      payment_type: "ACCOUNT",
-      amount: "30000",
-      repeat_kind: "WEEK",
-      exclude: false,
-      fix_amount: false,
-    },
-  ];
-  // schedules?.filter(
-  //   (s) => s.event_name === eventName && s.price_type === priceType
-  // ) ?? [];
 
   const pickDate = async () => {
     const newMonth = await openDayPeriodPicker(period.start, period.end);
@@ -86,8 +40,6 @@ const useRegularAsset = () => {
   return {
     spendSchedules,
     saveSchedules,
-    detailSchedules,
-    detailSchedule: detailSchedules[0],
     eventName,
     category,
     templateCount,
