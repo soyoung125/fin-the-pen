a3f178d7ab9c8d41a88cffbd8109cc677bd3feb9
soyoung125 <sypark041754@gmail.com>
2024-10-08 18:10:22 +0900

    refactor: prevent users not logged in

--- src/components/layouts/containerLayout/HomeLayout.tsx
+++ src/components/layouts/containerLayout/HomeLayout.tsx
@@ -1,12 +1,24 @@
 import Box from "@mui/material/Box";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import BottomBar from "../common/BottomBar.tsx";
 import { useAppSelector } from "@redux/hooks.ts";
 import { selectBottomBarOpen } from "@redux/slices/commonSlice.tsx";
 import OverlayProvider from "@hooks/use-overlay/OverlayProvider.tsx";
+import { useUser } from "@app/tanstack-query/useUser.ts";
+import { useEffect } from "react";
 
 export default function HomeLayout() {
+  const navigate = useNavigate();
+
   const bottomBarOpen = useAppSelector(selectBottomBarOpen);
+
+  const { data: user } = useUser();
+
+  useEffect(() => {
+    if (!user || Object.keys(user).length === 0) {
+      return navigate("/");
+    }
+  }, [user]);
 
   return (
     <OverlayProvider>
