d116653c3cdc7455b01802643208ad98667ccf67
soyoung125 <sypark041754@gmail.com>
2024-11-07 13:51:25 +0900

    refactor: change signin check token refresh to access

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -7,17 +7,17 @@ import { HEADER_MODE } from "@app/types/common.ts";
 import { useUser } from "@app/tanstack-query/useUser.ts";
 import { Link } from "react-router-dom";
 import { getCookie } from "@utils/storage.ts";
-import { COOKIE_KEY_REFRESH_TOKEN } from "@api/keys.ts";
+import { COOKIE_KEY_ACCESS_TOKEN } from "@api/keys.ts";
 
 function SignIn() {
   const { data: user } = useUser();
-  const refreshToken = getCookie(COOKIE_KEY_REFRESH_TOKEN);
+  const accessToken = getCookie(COOKIE_KEY_ACCESS_TOKEN);
 
   useHeader(true, HEADER_MODE.sign);
 
   return (
     <Stack justifyContent="center" alignItems="center" px={2.5} spacing={3}>
-      {!refreshToken ? ( // 버그 수정 필요
+      {!accessToken ? ( // 버그 수정 필요
         <>
           <Header />
           <SignInFields />
