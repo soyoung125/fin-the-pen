f09497231e0198840b2d24a0536563cceaac3231
soyoung125 <sypark041754@gmail.com>
2024-11-21 15:32:35 +0900

    fix: change api proxy

--- src/app/api/url.ts
+++ src/app/api/url.ts
@@ -2,7 +2,4 @@
  * 반드시 슬래시가 없는 상태로 끝내기
  */
 
-export const DOMAIN =
-  process.env.NODE_ENV === "production"
-    ? import.meta.env.VITE_LOCAL_DOMAIN
-    : "/local"; // 로컬 서버와 연결할 때 (단, MSW 동작 시 로컬 서버로 요청이 나가지 않고 mocking 당할 수 있음)
+export const DOMAIN = process.env.NODE_ENV === "production" ? "api" : "/local"; // 로컬 서버와 연결할 때 (단, MSW 동작 시 로컬 서버로 요청이 나가지 않고 mocking 당할 수 있음)
